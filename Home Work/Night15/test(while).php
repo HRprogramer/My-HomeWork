b373441434b9740b66fbb4db2d36ff22b39e66e8
Mute <152711267+HRprogramer@users.noreply.github.com>
2024-01-15 12:47:01 +0330

    Add Night15 (While & If)

---
+++ Home Work/Night15/test(while).php
@@ -0,0 +1,19 @@
+<?php
+
+$scores= array(15,17,9);
+$i =0;
+while($i <3){
+
+    if($scores[%i] <10){
+
+        echo "fail";
+    }
+    else{
+        
+        echo "passed";
+    }
+    $i++;
+}
+
+
+?>
